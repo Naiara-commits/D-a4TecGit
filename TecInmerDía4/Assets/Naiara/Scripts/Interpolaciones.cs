1789dab0c97068f593299be3264d5cb7182e2fd3
Naiara <naiara_igartua@euneiz.com>
2026-03-04 17:47:41 +0100

    Color y rotación

--- TecInmerDía4/Assets/Naiara/Scripts/Interpolaciones.cs
+++ TecInmerDía4/Assets/Naiara/Scripts/Interpolaciones.cs
@@ -9,6 +9,14 @@ public class Interpolaciones : MonoBehaviour
     [SerializeField] public float moveDuration = 5.0f;
     [SerializeField] private float moveTimer = 0;
 
+
+    [SerializeField] public Light nLuz;
+    [SerializeField] public Color startColor = Color.red;
+    [SerializeField] public Color endColor = Color.blue;
+
+    [SerializeField] public Vector3 intRot = Vector3.zero;
+    [SerializeField] public Vector3 endPosRot = new Vector3(0, 100, 0);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,7 +28,11 @@ public class Interpolaciones : MonoBehaviour
     {
         moveTimer += Time.deltaTime;
         float t = moveTimer / moveDuration;
+
         cube.transform.position = Vector3.Lerp(intPos, endPos, t);
+        nLuz.color = Color.Lerp(startColor, endColor, t);
+
+        cube.transform.rotation = Quaternion.Slerp(Quaternion.Euler(intRot), Quaternion.Euler(endPosRot), t);
         
     }
 }
