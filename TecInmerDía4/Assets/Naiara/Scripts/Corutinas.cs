dd6e71f9e7bc4459979765b60d39a33220ecc8da
Naiara <naiara_igartua@euneiz.com>
2026-03-04 18:03:38 +0100

    Comentarios

--- TecInmerDía4/Assets/Naiara/Scripts/Corutinas.cs
+++ TecInmerDía4/Assets/Naiara/Scripts/Corutinas.cs
@@ -7,9 +7,11 @@ public class Corutinas : MonoBehaviour
     void Start()
     {
         Debug.Log("Paso1");
-        StartCoroutine(FuncionEjemplo());
+        StartCoroutine(FuncionEjemplo());       //Empezar la corutina
         Debug.Log("Paso2");
         Invoke(nameof(FuncionInvocable), 2.0f);
+        InvokeRepeating(nameof(FuncionInvocable),4.0f, 1.0f); //Primer tiempo y luego cada cuanto se repite por eso se hace el repeating
+        Debug.Log("Paso3");
     }
 
     // Update is called once per frame
@@ -20,8 +22,9 @@ public class Corutinas : MonoBehaviour
 
     private IEnumerator FuncionEjemplo()
     {
-        //yield return new WaitForSeconds(3f);
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        //yield return new WaitForSeconds(3f);          //Aqui esperas unos segundos
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));  //Aqui esperas a que le des al espacio
+        CancelInvoke(nameof(FuncionInvocable));         //Para la invocación del "Hola"
         Debug.Log("He entrado en la función");
 
     }
