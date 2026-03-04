0a93dc4d7a07c396ebb142317a2d5e6145d08e69
Naiara <naiara_igartua@euneiz.com>
2026-03-04 17:36:43 +0100

    Cubo pasa de posición escena collisiones

--- TecInmerDía4/Assets/Naiara/Scripts/Interpolaciones.cs
+++ TecInmerDía4/Assets/Naiara/Scripts/Interpolaciones.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Interpolaciones : MonoBehaviour
 {
+    [SerializeField] public GameObject cube;
+
+    [SerializeField] public Vector3 intPos = Vector3.zero;
+    [SerializeField] public Vector3 endPos = new Vector3(10, 0, 0);
+    [SerializeField] public float moveDuration = 5.0f;
+    [SerializeField] private float moveTimer = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,9 @@ public class Interpolaciones : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveTimer += Time.deltaTime;
+        float t = moveTimer / moveDuration;
+        cube.transform.position = Vector3.Lerp(intPos, endPos, t);
         
     }
 }
