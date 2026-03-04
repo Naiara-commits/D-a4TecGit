86c9e349f9c4df0c3b6b7d290e0cecb7e26db477
Naiara <naiara_igartua@euneiz.com>
2026-03-04 17:27:16 +0100

    Script y escena interpolaciones

---
+++ TecInmerDía4/Assets/Naiara/Scripts/Interpolaciones.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Interpolaciones : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
